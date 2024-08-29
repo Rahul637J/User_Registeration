a3c8012d639185c1e6f57602b64677229e174293
Your Name <you@example.com>
2024-08-29 22:55:17 +0530

    [Rahul] Add : Python program for validate user last name test cases

--- unit_test.py
+++ unit_test.py
@@ -26,14 +26,39 @@ class ValidatingUserCredentials(unittest.TestCase):
             None
         '''
         
-         self.assertTrue(check_name_format('Rahul'))
-         self.assertTrue(check_name_format('Rah'))
-         self.assertTrue(check_name_format('Rahugl'))
-         self.assertFalse(check_name_format('ra'))
-         self.assertFalse(check_name_format('rahul'))
-         self.assertFalse(check_name_format('RAHUL'))
-         self.assertFalse(check_name_format('r'))
-         self.assertFalse(check_name_format('rAH'))
+         self.assertTrue(check_name_format('Rahul'),"Should be valid")
+         self.assertTrue(check_name_format('Rah'),"Should be valid")
+         self.assertTrue(check_name_format('Rahugl'),"Should be valid")
+         self.assertFalse(check_name_format('ra'),"Should not be valid due invalid length and 1st character lower case")
+         self.assertFalse(check_name_format('rahul'),"Should not be valid due to 1st character lower case")
+         self.assertFalse(check_name_format('RAHUL'),"Should not be valid due to all characters are upper case")
+         self.assertFalse(check_name_format('r'),"Should not be valid due to invalid size and invalid 1st character")
+         self.assertFalse(check_name_format('rAH'),"Should not be valid dur to invalid all characters")
+    
+    def test_last_name(self):
+        
+         '''
+        Description: 
+            The function tests the `check_name_format()` function by checking 
+            various name inputs for valid and invalid formats. It asserts that 
+            names starting with a capital letter followed by at least two lowercase 
+            letters return True, while invalid formats return False.
+            
+        Parameters:
+            self (unittest.TestCase): The instance of the test case class 
+            containing the test methods.
+            
+        Return:
+            None
+        '''
+        
+         self.assertTrue(check_name_format('Jag'),"Should be valid")
+         self.assertTrue(check_name_format('Jaganathan'),"Should be valid")
+         self.assertFalse(check_name_format('ja'),"Should not be valid due to invalid size and 1st character")
+         self.assertFalse(check_name_format('jaH'),"Should not be valid due to 1st character lower case")
+         self.assertFalse(check_name_format('rahul'),"Should not be valid due to 1st character lower case")
+         self.assertFalse(check_name_format('RAHUL'),"Should not be valid due to all characters arer upper case")
+         self.assertFalse(check_name_format('r'),"Should not be valid due to invalid size") 
 
 def main():
     obj=ValidatingUserCredentials

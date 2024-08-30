68b8eb3a9cbb6a145e930e4a3db820d82eb2008c
Your Name <you@example.com>
2024-08-30 20:11:27 +0530

    [Rahul] Added: Python program for validating password

--- User_Registration.py
+++ User_Registration.py
@@ -47,9 +47,8 @@ def check_phone_number_format(phone_number):
     
     '''
     Description: 
-        The function checks if the given name follows the specified format, 
-        where the first character must be an uppercase letter followed by at 
-        least two lowercase letters.
+        The function checks if the given phone number follows the specified format, 
+        where the format start's with contry code followed by space and 10 digits.
     Parameters:
         name (str): The name to be checked.
     Return:
@@ -59,6 +58,22 @@ def check_phone_number_format(phone_number):
     
     pattern="^[0-9]{2} [0-9]{10}$"
     return re.fullmatch(pattern,phone_number)
+
+def check_password_format(password):
+    
+    '''
+    Description: 
+        The function checks if the given password follows the specified format, 
+        where the format is atleast 8 characters .
+    Parameters:
+        name (str): The name to be checked.
+    Return:
+        re.Match object or None: Returns a match object if the name follows 
+        the format, or None if it doesn't.
+    '''
+    
+    pattern="[A-za-z0-9]{8,}"
+    return re.fullmatch(pattern,password)
 
 def get_valid_name(prompt):
     
@@ -89,7 +104,7 @@ def get_valid_email(msg):
     
     '''
     Description: 
-        Prompts the user to input a name and validates it using the check_name_format 
+        Prompts the user to input a email and validates it using the check_eamil_format 
         function. The user gets up to 3 attempts to input a valid name.
     Parameters:
         prompt (str): The input prompt message for the user.
@@ -113,7 +128,7 @@ def get_valid_phone_number(msg):
     
     '''
     Description: 
-        Prompts the user to input a name and validates it using the check_name_format 
+        Prompts the user to input a phone number and validates it using the check_phone_number_format 
         function. The user gets up to 3 attempts to input a valid name.
     Parameters:
         prompt (str): The input prompt message for the user.
@@ -132,6 +147,31 @@ def get_valid_phone_number(msg):
         else:
             print(f'{attempts} out of 3 attempts used.')
             attempts+=1
+
+def get_valid_password(msg):
+    
+    '''
+    Description: 
+        Prompts the user to input a name and validates it using the check_name_format 
+        function. The user gets up to 3 attempts to input a valid name.
+    Parameters:
+        prompt (str): The input prompt message for the user.
+    Return:
+        none
+    '''
+    
+    attempts=1
+    
+    while attempts<=3:
+        password=input(msg)
+        
+        if check_password_format(password):
+            return password
+        
+        else:
+            print(f'{attempts} out of 3 attempts used.')
+            attempts+=1
+                
                 
 def main():
     first_name = get_valid_name("Enter your First name (First letter Capital): ")
@@ -146,19 +186,25 @@ def main():
                 phone_number=get_valid_phone_number("Enter your phone number: ")
 
                 if phone_number:
-                  logger_init(phone_number).info("User Registration successful!!!")    
+                    password=get_valid_password("Enter the password (minimum 8 characters): ")
+                    
+                    if password:
+                        logger_init(password).info("User registered successfull!!")
+                    
+                    else:
+                        logger_init(password).warning("Session expired, Register again!!!")    
                 
                 else:
-                    logger_init(phone_number).warning("Registration Expired, Register again!!!") 
+                    logger_init(phone_number).warning("Session Expired, Register again!!!") 
             
             else:
-                logger_init(email).warning("Registration expired, Register again!!!")    
+                logger_init(email).warning("Session expired, Register again!!!")    
         
         else:
-            logger_init(last_name).warning("Registration expired, Register again!!!")
+            logger_init(last_name).warning("Session expired, Register again!!!")
                  
     else:
-        logger_init(first_name).warning("First name invalid, Registration failed")
+        logger_init(first_name).warning("Session expired, Registration failed")
     
 if __name__ == "__main__":
     main()

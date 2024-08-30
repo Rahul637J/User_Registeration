a90ec7e4c7478fc38ce38f09a5ef058c61a9c629
Your Name <you@example.com>
2024-08-30 10:51:57 +0530

    [Rahul] Add: Python program to validate user 1st name

--- User_Registration.py
+++ User_Registration.py
@@ -6,7 +6,9 @@
 @Title: Employee wages - Python program to validate user input for registration
 '''
 
+from MyLogging import logger_init
 import re 
+
 
 def check_name_format(name):
     
@@ -26,24 +28,42 @@ def check_name_format(name):
     
      pattern="^[A-Z][a-z]{2,}"
      return re.match(pattern,name)
+ 
+def get_valid_name(prompt):
+    
+    '''
+    Description: 
+        Prompts the user to input a name and validates it using the check_name_format 
+        function. The user gets up to 3 attempts to input a valid name.
+    Parameters:
+        prompt (str): The input prompt message for the user.
+    Return:
+        str or None: Returns the valid name if successful, otherwise None if validation fails after 3 attempts.
+    '''
+    
+    attempts = 1
+    name=prompt
+    while attempts <= 3:
+        if check_name_format(name):
+            return name
+        else:
+            print(f'{attempts} out of 3 attempts used.')
+            attempts += 1
+            name = input("Enter the valid name: ")
+
+    return None
 
 def main():
     
-    attempts=1
+    first_name=input("Enter your first name (1st character should capital): ")
+    valid_name=get_valid_name(first_name)
     
-    while attempts<=3:
-        first_name=input("Enter your name First name (First letter Capital): ")
-        result=check_name_format(first_name)
-
-        if result:
-            print("Name is Valid") 
-        else:
-            print("Invalids Name")    
-            print(f'{attempts} out of 3')
-            attempts+=1
-            
-    if attempts>3:
-        print("Registration Expired start new registration!!")        
+    if valid_name:
+        logger_init(valid_name).info("User registration  successfull")
+    
+    else:
+        logger_init(valid_name).warning("User registration expired, Register again")    
+           
         
 if __name__=="__main__":
     main()
